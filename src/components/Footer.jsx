177a258e9384992101de81962a0cf8197791e42c
Alkesh <acealkesh@gmail.com>
2021-09-10 00:27:24 +0530

    Added Git url repo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <nav className="navbar fixed-bottom navbar-dark bg-dark ">
       <div className="container topnav-centered text-centre">
-        <a className="navbar-brand mx-5" href="https://www.google.com">
+        <a className="navbar-brand mx-5" target="_blank" rel="noreferrer" href="https://github.com/AlkeshKothar/React-TodoList">
           FOR SOURCE CODE, CLICK AND FOLLOW ME ON : GITHUB
         </a>
       </div>
